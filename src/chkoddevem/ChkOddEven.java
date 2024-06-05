7955220ade14cb3de199bb19a36f6005c0b23b87
Aluno <Aluno@10.85.198.36>
2024-06-05 16:35:01 -0300

    churek

---
+++ src/chkoddevem/ChkOddEven.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+
+
+    public static void main(String args[]) {
+
+        Scanner in = new Scanner(System.in);
+        int num = 0;
+        System.out.println("Enter a number:");
+        num = in.nextInt();
+        
+    }
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+
+
+/**
+ *
+ * @author Aluno
+ */
+
+
+    /**
+     * @param args the command line arguments
+     */
+  
+        // TODO code application logic here
+    
+   
+
+class InputStream {
+    
+}
